002c61761a552b1c359502ee99f4cf0bc58eabba
mika-yan <kryuger.mikaelyan@inbox.ru>
2020-06-17 16:55:12 +0300

    Updates

---
+++ precache-manifest.831dff56ff9f77a082722e155e77058b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3298fba9bdcee052c2fb669fa93685",
+    "url": "/vkminiapptest/index.html"
+  },
+  {
+    "revision": "260a03d9d2840aecc197",
+    "url": "/vkminiapptest/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ef8abb424523919694b5",
+    "url": "/vkminiapptest/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "260a03d9d2840aecc197",
+    "url": "/vkminiapptest/static/js/2.d7d65a76.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vkminiapptest/static/js/2.d7d65a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8abb424523919694b5",
+    "url": "/vkminiapptest/static/js/main.f4de2461.chunk.js"
+  },
+  {
+    "revision": "b89860b467a9443cd540",
+    "url": "/vkminiapptest/static/js/runtime-main.ef66bb5a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkminiapptest/static/media/persik.4e1ec840.png"
+  }
+]);
